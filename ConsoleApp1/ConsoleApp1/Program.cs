01ec38c92e4d7953162dfe04a86608ecbff4e395
Ludmila <luvi_lv@inbox.lv>
2018-05-10 19:01:05 +0300

    5.2
    Ludmila

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ka Tevi sauc?");//izvade
+            Console.ReadLine();//ievade
+
+            Console.WriteLine("Ludmila");//izvade
+            Console.ReadLine();//ievade
+
+        }
+    }
+}
