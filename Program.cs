a089a7e5e7a290ace88c7f5e8b86ebc83d3fb20d
Ludmila <luvi_lv@inbox.lv>
2018-05-10 18:57:16 +0300

    5.1
    Ka Tevi saus

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ka Tevi sauc?");//izvade
+            Console.ReadLine();//ievade
+
+            
+        }
+    }
+}
