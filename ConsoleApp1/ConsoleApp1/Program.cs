ddfc7b9ccc8d8e08a661b351fe8d02811c410638
Ludmila <luvi_lv@inbox.lv>
2018-05-10 20:47:00 +0300

    5.3.
    pt_1_1 (1,2)

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,12 +10,43 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string dati;
             Console.WriteLine("Ka Tevi sauc?");//izvade
-            Console.ReadLine();//ievade
+            dati=Console.ReadLine();//ievade
+            Console.WriteLine(dati);
 
-            Console.WriteLine("Ludmila");//izvade
+            Console.WriteLine("Kāds it tagad gads?");//izvade
+            string mainigais;
+            mainigais=Console.ReadLine();
+            int skaitlis = Convert.ToInt16(mainigais);
+
+            Console.WriteLine("Kads Tavs ir dzimšanas datums?");//izvade
+            string mainigais2;
+            mainigais2 = Console.ReadLine();
+            int skaitlis2 = Convert.ToInt16(mainigais2);
+
+            Console.WriteLine("Cik Tev gadu?");//izvade
+
+
+
+            int skaitlis3 = skaitlis - skaitlis2;
+            Console.WriteLine(skaitlis3);
+            
             Console.ReadLine();//ievade
+        }
+        static void FunkcijaDiametrs()//diametrs
+        {
+            
+           
+        }
+        static void FunkcijaLaukums()//laukums
+        {
+
 
         }
+        
+
+
+        
     }
 }
